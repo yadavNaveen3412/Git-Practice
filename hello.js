0c3159c1574e6cc6387602cd33da1cbe765771dd
Naveen Yadav <naveen.yadav@argusoft,com>
2025-07-29 11:42:06 +0530

    Studying js

---
+++ hello.js
@@ -0,0 +1,27 @@
+let walk =['n','n','n','s','n','s','n','s','n','s'];
+
+function isValidWalk(walk) {
+  let x = 0;
+  let y = 0;
+    if(walk[item] === 'n'){
+console.log(x, " ", y);
+        y += 1;
+    }
+    else if(walk[item]=== 's'){
+console.log(x, " ", y);
+        y -= 1;
+    }
+    else if(walk[item]=== 'e'){
+console.log(x, " ", y);
+        x += 1;
+    }
+    else if (walk[item]=== 'w'){
+console.log(x, " ", y);
+        x -= 1;
+    }
+  }
+  
+  return (x === 0 && y === 0 && walk.length === 10);  
+}
+
+console.log(isValidWalk(walk));
---
+++ index.js
@@ -0,0 +1,2 @@
+let a = '456979865';
+console.log(a);
